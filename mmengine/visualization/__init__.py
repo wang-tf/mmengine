6b10a68e846de04cb5691541a64fcd9b93772ab9
WANG Tengfei <ternencewang2015@outlook.com>
2023-01-10 20:29:30 +0800

    [Enhance] add DvcliveVisBackend
    [Enhance] add DvcliveVisBackend in path

--- mmengine/visualization/__init__.py
+++ mmengine/visualization/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .vis_backend import (BaseVisBackend, LocalVisBackend,
-                          TensorboardVisBackend, WandbVisBackend)
+                          TensorboardVisBackend, WandbVisBackend,
+                          DvcliveVisBackend)
 from .visualizer import Visualizer
 
 __all__ = [
     'Visualizer', 'BaseVisBackend', 'LocalVisBackend', 'WandbVisBackend',
-    'TensorboardVisBackend'
+    'TensorboardVisBackend', 'DvcliveVisBackend'
 ]
